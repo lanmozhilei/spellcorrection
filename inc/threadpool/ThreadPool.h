92aa18d950cb2c769eb1196633082941c924c2eb
lanmozhilei <891915038@qq.com>
2016-02-25 22:31:23 -0800

    add include ThreadPool.h

---
+++ inc/threadpool/ThreadPool.h
@@ -0,0 +1,21 @@
+#ifndef _WD_THREADPOOL_H
+#define _WD_THREADPOOL_H
+
+#include "Noncopyable.h"
+#include "MutexLock.h"
+#include "Condition.h"
+#include "Thread.h"
+
+#include <queue>
+#include <vector>
+#include <functional>
+#include <memory>
+
+namespace wd
+{
+class MyDict;
+class Task;
+
+
+}//end of namesapce
+#endif
