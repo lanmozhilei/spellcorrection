bb5b9ed515229906940cd87f83991019fbf6636a
lanmozhilei <891915038@qq.com>
2016-02-28 23:16:56 -0800

    add index .cc .h

--- src/MyDict.cc
+++ src/MyDict.cc
@@ -1,5 +1,8 @@
 #include "MyDict.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
+
 
 namespace wd
 {
@@ -49,7 +52,92 @@ void MyDict::show_index_table()
 	}
 }
 
-void 
+void read_from(const char* dictpath)
+{
+	std::ifstream in(dictpath);//open to read
+	if(!in)
+	{
+		std::cout << __DATE__ << " " << __TIME__
+				  << __FILE__ << " " << __LINE__
+				  << ": dict read error" << std::endl;
+		exit(-1);
+	}
+
+	std::string line;
+	while(getline(in,line))
+	{
+		std::stringstream ss(line);
+		std::string key;
+		int value;
+		ss >> key >> value;
+		dict_push_back(make_pair(key,value));
+	}
+	in.close;
+}
+
+//provate:
+MyDict::MyDict(const char* dictpath, const char* dictpath2)
+{
+	read_from(dictpath);
+	read_from(dictpath2);
+	//创建索引表
+	for(size_t idx = 0; idx != dict_.size();++idx)
+	{
+		record_to_index(idx);
+	}
+}
+
+void MyDict::record_to_index(int idx)//建立索引
+{
+	std::string key;
+	std::string word = dict_[idx].first;
+	for(std::size_t iidx = 0; iidx != word.size(); ++iidx)
+	{
+		char ch = word[iidx];
+		if(ch & (1 << 7))
+		{
+			if((ch & 0xF0) == 0xC0 || (ch & 0xF0) == 0xD0)
+			{
+				key = word.substr(iidx, 2);
+				++iidx;
+			}
+			elseif((ch & 0xF0) == 0xE0)
+			{
+				key = word.substr(iidx, 3);
+				iidx += 2;
+			}
+			else if((ch & 0xFF) == 0xF0)
+			{
+				key = word.substr(iidx, 4);
+				iidx += 3;
+			}
+			else if((ch & 0xFF) == 0xF8)
+			{
+				key = word.substr(iidx, 5);
+				iidx += 4;
+			}
+			else if((ch & 0xFF) == 0xF8)
+			{
+				key = word.substr(iidx, 5);
+				iidx += 4;
+			else if((ch & 0xFF) == 0xF8)
+			{
+				key = word.substr(iidx, 5);
+				iidx += 4;
+			}
+			else if((ch & 0xFF) == 0xFE)
+			}
+				key = word.substr(iidx, 6);
+				iidx += 5;
+			}
+		}
+		else
+		{
+			key = word.substr(iidx,1);
+		}
+		index_table_[key].insert(idx);
+	}
+}
 }//end o namespace
 
 
