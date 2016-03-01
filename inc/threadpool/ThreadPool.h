2c7dc327d62275512929523057033df117225930
lanmozhilei <891915038@qq.com>
2016-02-29 20:34:29 -0800

    add Cache.cc

--- inc/threadpool/ThreadPool.h
+++ inc/threadpool/ThreadPool.h
@@ -16,6 +16,36 @@ namespace wd
 class MyDict;
 class Task;
 
+class ThreadPool : private Noncopyable
+{
+public:
+	ThreadPool(size_t queueSize, size_t threadsNum, Cache &cache, MyDict &mydict);
+
+	void start();
+
+	void addTask();
+	Task getTask();
+
+	void runInThread(Cache &cache);
+
+	void update();//更新Cache
+
+private:
+	mutable MutexLock mutex_;
+	Condition empty_;
+	Condition full_;
+
+	size_t queueSize_;
+	std::queue<Task> queue_;
+
+	const size_t theadsNum_;
+	std::vector<std::unique_ptr<Thread> > threads_;
+	bool isExit_;
+
+	Cache &cache_;
+public:
+	MyDict &mydict_;
+};
 
 }//end of namesapce
 #endif
